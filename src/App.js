17ee959c768664aea40bf3f93a6a79be6e351b0b
Igor Redchuk <redchuk@gmail.com>
2019-09-11 09:46:51 +0200

    Cleanup

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { ValidationProvider } from "./components/validation-context";
 import Form from "./components/form";
 import validateForm from "./services/validate-form";
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ValidationProvider validator={validateForm}>
-          <Form initialPostalCode="" initialCity="Munich"></Form>
-        </ValidationProvider>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <ValidationProvider validator={validateForm}>
+        <Form initialPostalCode="" initialCity="Munich"></Form>
+      </ValidationProvider>
+    </header>
+  </div>
+);
+
+export default App;
